a567276f9387ff72f6c668abdbcd3e5a9f8d1cf3
Yermukhamed Shakhman <ermukhamed.shakhman@gmail.com>
2025-06-08 07:10:39 +0200

    pass

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.getElementById("burger");
+  const navLinks = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+  });
+});
+
 document.getElementById("contactForm").addEventListener("submit", function (e) {
     e.preventDefault();
     alert("Thanks for reaching out! We'll get back to you shortly.");
